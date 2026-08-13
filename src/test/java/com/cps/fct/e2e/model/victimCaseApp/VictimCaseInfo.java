17e9dc16ee6c5d748fbabaa2bb0faf5c8b03da45
Murali.Govindachar <murali.govindachar@cps.gov.uk>
2026-08-13 17:57:23 +0000

    Tidy Up

---
+++ src/test/java/com/cps/fct/e2e/model/victimCaseApp/VictimCaseInfo.java
@@ -0,0 +1,18 @@
+package com.cps.fct.e2e.model.victimCaseApp;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+
+public class VictimCaseInfo {
+
+    private String Urn;
+    private String CreatedBy;
+
+
+
+
+
+}
